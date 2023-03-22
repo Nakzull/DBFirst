20c95f768c0218c20adcda2eff4a65b3e5521960
Nakzull <andr25f4@zbc.dk>
2023-03-22 10:55:07 +0100

    Add project files.

---
+++ DBFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BestBioEntities2())
+            {
+                var query = context.Get_User_Data("mra@stofo.dk").FirstOrDefault();
+
+                Console.WriteLine(query.Name + " " + query.Address + " " + query.PhoneNumber);
+                Console.ReadKey();
+            }
+        }
+    }
+}
